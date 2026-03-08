18e64c7b8b6ca38f769b019e75b4654042995ced
doouuiiee <doouuiiee@gmail.com>
2026-03-09 00:53:24 +0800

    Add files via upload
    EMAIL_HOST=sandbox.smtp.mailtrap.io
    
    EMAIL_PORT=2525
    
    EMAIL_USER=d512388f18eab2
    
    EMAIL_PASS=****1224

--- server.js
+++ server.js
@@ -92,7 +92,8 @@ app.use('/api/upload', uploadRoutes);
 // Email transporter configuration
 const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: process.env.EMAIL_HOST || 'sandbox.smtp.mailtrap.io',
+  port: process.env.EMAIL_PORT || 2525,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
